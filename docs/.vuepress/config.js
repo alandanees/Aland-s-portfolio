e7be9fb5cd693079a8425ad512e7896122b3e7fe
Aland Anees <aland.anees@gmail.com>
2025-10-06 03:40:48 +0300

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'en-US',
+
+  title: 'Aland\'s Portfolio',
+  description: 'Aland Anees - Software Engineering Student & Frontend Developer',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: [
+      {
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'About',
+        link: '/about/'
+      },
+      {
+        text: 'Projects',
+        link: '/projects/'
+      },
+      {
+        text: 'Contact',
+        link: '/contact/'
+      }
+    ],
+  }),
+
+  bundler: viteBundler(),
+})

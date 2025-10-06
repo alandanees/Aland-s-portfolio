dcb503106adf71c828eb494009df6374f874743c
Aland Anees <aland.anees@gmail.com>
2025-10-06 04:25:23 +0300

    Fix base path configuration

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,9 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
-
+  base: '/Aland-s-portfolio/',  // Must match your repo name exactly
+  ignoreDeadLinks: true,
+  
   title: 'Aland\'s Portfolio',
   description: 'Aland Anees - Software Engineering Student & Frontend Developer',
 
